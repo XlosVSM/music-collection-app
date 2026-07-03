4f53722eac7a529fd8504a944e14aaab5a7a48c2
Matthew McDermott <matthewmcd07@gmail.com>
2026-07-03 20:24:38 +1200

    Added tracklist variable

--- MusicCollection/src/xlosvsm/database/CD.java
+++ MusicCollection/src/xlosvsm/database/CD.java
@@ -1,10 +1,24 @@
 package xlosvsm.database;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Matthew McDermott
  */
 public record CD(int amount) implements Medium {
 	public CD {
 		assert amount > 0;
+	}
+
+	@Override
+	public List<List<String>> tracklist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTracklist(Map<String, Integer> tracklist) {
+		// TODO Auto-generated method stub
 	}
 }
--- MusicCollection/src/xlosvsm/database/Vinyl.java
+++ MusicCollection/src/xlosvsm/database/Vinyl.java
@@ -1,6 +1,7 @@
 package xlosvsm.database;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @param diameter
@@ -9,17 +10,28 @@ import java.util.List;
  * @param colour
  * @author Matthew McDermott
  */
-public record Vinyl(int diametre, int RPM, int amount, String colour) implements Medium {
+public record Vinyl(int diameter, int RPM, int amount, String colour) implements Medium {
 	public Vinyl {
-		List<Integer> vinylDiametres = List.of(3, 7, 10, 12);
-		List<Integer> vinylRPMs = List.of(33, 45, 78);
+		List<Integer> vinylDiameters = List.of(3, 7, 10, 12);
+		List<Integer> vinylRPMs = List.of(16, 33, 45, 78);
 
-		assert vinylDiametres.contains(diametre);
+		assert vinylDiameters.contains(diameter);
 		assert vinylRPMs.contains(RPM);
 		assert amount > 0;
 	}
-	
+
 	public Vinyl(int diametre, int RPM, int amount) {
 		this(diametre, RPM, amount, "Black");
+	}
+
+	@Override
+	public List<List<String>> tracklist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTracklist(Map<String, Integer> tracklist) {
+		// TODO Auto-generated method stub
 	}
 }
