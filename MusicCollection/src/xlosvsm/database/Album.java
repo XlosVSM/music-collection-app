1effe7d7ab60c49997fab5530d4db27e59024898
Matthew McDermott <matthewmcd07@gmail.com>
2026-07-03 16:21:27 +1200

    Created start of database code

---
+++ MusicCollection/src/xlosvsm/database/Album.java
@@ -0,0 +1,4 @@
+package xlosvsm.database;
+
+public record Album(String name, String artist, Integer yearOfRelease, String publisher, String id) {
+}
---
+++ MusicCollection/src/xlosvsm/testing/DatabaseTest.java
@@ -0,0 +1,13 @@
+package xlosvsm.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DatabaseTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
