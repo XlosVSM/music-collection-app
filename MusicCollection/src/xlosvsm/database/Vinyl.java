1b431320bec82602f366a0d8fffd10d61c2428e9
Matthew McDermott <matthewmcd07@gmail.com>
2026-07-04 10:00:23 +1200

    Added first medium tests

--- MusicCollection/src/xlosvsm/database/Vinyl.java
+++ MusicCollection/src/xlosvsm/database/Vinyl.java
@@ -10,18 +10,28 @@ import java.util.Map;
  * @param colour
  * @author Matthew McDermott
  */
-public record Vinyl(int diameter, int RPM, int amount, String colour) implements Medium {
+public record Vinyl(int diameter, int RPM, String colour, int amount) implements Medium {
 	public Vinyl {
 		List<Integer> vinylDiameters = List.of(3, 7, 10, 12);
 		List<Integer> vinylRPMs = List.of(16, 33, 45, 78);
 
-		assert vinylDiameters.contains(diameter);
-		assert vinylRPMs.contains(RPM);
-		assert amount > 0;
+		if (!vinylDiameters.contains(diameter)) {
+			throw new IllegalArgumentException(
+					"Vinyl diameter must be one of the following sizes: " + vinylDiameters.toString());
+		}
+
+		if (!vinylRPMs.contains(RPM)) {
+			throw new IllegalArgumentException(
+					"Vinyl RPM must be one of the following amounts: " + vinylRPMs.toString());
+		}
+
+		if (amount <= 0) {
+			throw new IllegalArgumentException("Amount must be greater than 0.");
+		}
 	}
 
 	public Vinyl(int diametre, int RPM, int amount) {
-		this(diametre, RPM, amount, "Black");
+		this(diametre, RPM, "Black", amount);
 	}
 
 	@Override
--- MusicCollection/src/xlosvsm/testing/MediumTests.java
+++ MusicCollection/src/xlosvsm/testing/MediumTests.java
@@ -8,10 +8,26 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import xlosvsm.database.Album;
+import xlosvsm.database.CD;
+import xlosvsm.database.Cassette;
 import xlosvsm.database.Vinyl;
 
 class MediumTests {
 	// Cassette checks
+	@Test
+	public void cassetteTest1() {
+		assertThrows(IllegalArgumentException.class, () -> new Cassette(0));
+	}
+
 	// CD checks
+	@Test
+	public void cdTest1() {
+		assertThrows(IllegalArgumentException.class, () -> new CD(0));
+	}
+
 	// Vinyl checks
+	@Test
+	public void vinylTest1() {
+		assertThrows(IllegalArgumentException.class, () -> new Vinyl(0, 0, 0));
+	}
 }
