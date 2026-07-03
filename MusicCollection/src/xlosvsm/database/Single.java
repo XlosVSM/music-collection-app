51f6da27289e221f70cd5e44adc41c8b9738044e
Matthew McDermott <matthewmcd07@gmail.com>
2026-07-04 10:10:51 +1200

    Added variables to EP and Single

--- MusicCollection/src/xlosvsm/database/Single.java
+++ MusicCollection/src/xlosvsm/database/Single.java
@@ -1,5 +1,9 @@
 package xlosvsm.database;
 
-public record Single() implements Release {
+import java.time.LocalDate;
+import java.util.List;
+
+public record Single(String name, String artist, LocalDate releaseDate, Medium medium, String publisher, String id,
+		List<String> genres) implements Release {
 
 }
