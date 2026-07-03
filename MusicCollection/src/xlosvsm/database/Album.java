96d716d345350d2d8b5ca7e8395abbcb0414c72d
Matthew McDermott <matthewmcd07@gmail.com>
2026-07-03 16:54:49 +1200

    Added genres and obtainedDate
    Switched from using Integer for year to LocalDate to hold more information in one variable

--- MusicCollection/src/xlosvsm/database/Album.java
+++ MusicCollection/src/xlosvsm/database/Album.java
@@ -1,6 +1,27 @@
 package xlosvsm.database;
 
-public record Album(String name, String artist, Integer yearOfRelease, String publisher, String id) {
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * The {@code Album} represents an album to be stored in the database.
+ * 
+ * @param name
+ * @param artist
+ * @param releaseDate
+ * @param publisher
+ * @param id
+ * @param genres
+ * @param obtainedDate
+ * @author Matthew McDermott
+ */
+public record Album(String name, String artist, LocalDate releaseDate, String publisher, String id, List<String> genres,
+		LocalDate obtainedDate) {
+	// Date firstDate1 = new Date(int year, int month, int date);
+	public Album(String name, String artist, LocalDate releaseDate, String publisher, String id, String genre, LocalDate obtainedDate) {
+		this(name, artist, releaseDate, publisher, id, List.of(genre), obtainedDate);
+	}
+	
 	@Override
 	public String toString() {
 		return "Album Name: " + name;
--- MusicCollection/src/xlosvsm/testing/DatabaseTest.java
+++ MusicCollection/src/xlosvsm/testing/DatabaseTest.java
@@ -2,6 +2,9 @@ package xlosvsm.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import xlosvsm.database.Album;
@@ -9,9 +12,33 @@ import xlosvsm.database.Album;
 class DatabaseTest {
 	@Test
 	public void testAlbum1() {
-		Album album = new Album("Amnesiac", null, null, null, null);
+		Album album = new Album("Amnesiac", null, null, null, null, List.of(), null);
 		String result = album.toString();
-		
+
+		assertEquals("Album Name: Amnesiac", result);
+	}
+
+	@Test
+	public void testAlbum2() {
+		Album album = new Album("Amnesiac", "Radiohead", null, null, null, List.of(), null);
+		String result = album.toString();
+
+		assertEquals("Album Name: Amnesiac", result);
+	}
+
+	@Test
+	public void testAlbum3() {
+		Album album = new Album("Amnesiac", "Radiohead", null, null, null, "Alternative Rock", null);
+		String result = album.toString();
+
+		assertEquals("Album Name: Amnesiac", result);
+	}
+	
+	@Test
+	public void testAlbum4() {
+		Album album = new Album("Amnesiac", "Radiohead", null, null, null, List.of("Alternative Rock"), LocalDate.of(2026, 7, 3));
+		String result = album.toString();
+
 		assertEquals("Album Name: Amnesiac", result);
 	}
 }
