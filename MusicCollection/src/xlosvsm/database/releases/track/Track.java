db73b657a60aed45f81db4576ef2f23c3cc5b55c
Matthew McDermott <matthewmcd07@gmail.com>
2026-07-13 19:10:19 +1200

    Added Track toString

--- MusicCollection/src/xlosvsm/database/releases/track/Track.java
+++ MusicCollection/src/xlosvsm/database/releases/track/Track.java
@@ -1,5 +1,8 @@
 package xlosvsm.database.releases.track;
 
 public record Track(String title, Duration duration, TrackPersonnel personnel) {
-
+	@Override
+	public String toString() {
+		return title + "(" + duration.toString() + ")";
+	}
 }
